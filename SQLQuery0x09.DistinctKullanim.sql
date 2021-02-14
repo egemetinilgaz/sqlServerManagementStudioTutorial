9cb4049261d3f6ef23ae6a0b3b1fc235f06af637
Ege Metin ILGAZ <59531182+egemetinilgaz@users.noreply.github.com>
2021-02-14 13:09:17 +0300

    SQLQuery0x09.DistinctKullanim.sql

---
+++ SQLQuery0x09.DistinctKullanim.sql
@@ -0,0 +1,26 @@
+use AdventureWorks2017
+--distinct kullanimi
+select distinct Color from Production.Product
+--Production.Product icerisinde color kolonunu listeler
+
+--once where icerisindeki sorgu calisacak
+--daha sonra
+--select ProductID, ProductNumber, Name, Color from Production.Product calisacak
+select ProductID, ProductNumber, Name, Color from Production.Product
+where
+ProductID in --asagidaki kisim, productIDin'in bir parametresi oldu
+--artik ana sorgudaki productID kismi degil burasi calisacak
+-- ProductNumber, Name, Color bunlar normal calismaya devam edecek
+(
+select distinct ProductID from Sales.SalesOrderDetail
+)
+
+--once select distinct ProductID from Sales.SalesOrderDetail calisir
+select Color, sum(ListPrice) from Production.Product
+where
+ProductID in
+(
+select distinct ProductID from Sales.SalesOrderDetail
+)
+group by Color
+having Color is not null
